22450a818fda079939bb30f14c9f37416f42a66b
sloan-j <sloanj205@gmail.com>
2018-07-11 00:16:26 -0400

    added search function

--- driver.py
+++ driver.py
@@ -6,4 +6,19 @@ reddit = praw.Reddit('bot1')
 
 class Meteorigon(object):
 
+    # search for a particular article within a subreddit
+    def search_titles(self): 
+        count = 0
+        user_sub = input("Which subreddit? ")
+        user_article = input("Name of article? ")
+        subreddit = reddit.subreddit(user_sub)
 
+        for submission in subreddit.hot(limit=20):
+            if(user_article in submission.title.lower()):
+                count+=1
+                print(count,": ", submission.title)
+                print("Comments: ", submission.num_comments)
+                print("Score: ", submission.score)
+                print("")
+
+        print(count, " articles found")
