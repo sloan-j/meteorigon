7afc08c5a97744698c885fbe0f419ec1a26a5209
sloan-j <sloanj205@gmail.com>
2018-07-13 01:45:56 -0400

    added return links function

--- driver.py
+++ driver.py
@@ -10,14 +10,14 @@ class Meteorigon(object):
         self.sub_storage = [] 
         self.driver = None
 
-    # search for a particular article within a subreddit
-    def search_titles(self): 
+    # search for articles containing key words
+    def search_titles(self):    	
         count = 0
         user_sub = input("Which subreddit? ")
         user_article = input("Name of article? ")
         subreddit = reddit.subreddit(user_sub)
 
-        for submission in subreddit.hot(limit=20):
+        for submission in subreddit.hot(limit=3):
             if(user_article in submission.title.lower()):
                 count+=1
                 print(count,": ", submission.title)
@@ -26,5 +26,10 @@ class Meteorigon(object):
                 print("URL: ", submission.url)
                 print("Comments: ", submission.permalink)
                 print("")
-
+               	self.sub_storage.insert(count-1, submission.permalink)
         print(count, " articles found")
+        print("")
+
+    # return links
+    def get_links(self):
+        return(self.sub_storage)
